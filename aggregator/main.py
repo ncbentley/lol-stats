36438826b44d87e918288be8e597f1febb1fd32e
NIco Bentley <ncbentley4@gmail.com>
2021-01-23 17:58:08 -0500

    runs on ECS successfully

--- aggregator/main.py
+++ aggregator/main.py
@@ -23,7 +23,7 @@ s = requests.Session()
 get_last_modified = lambda obj: int(datetime.timestamp(obj.last_modified))
 
 def get_next_player():
-    session = Session(profile_name="Default")
+    session = Session(aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"), aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"))
     s3 = session.resource("s3")
     bckt = s3.Bucket("lol-stats-players-queue")
     players = [player for player in bckt.objects.all()]
@@ -38,7 +38,7 @@ def get_next_player():
     return player
 
 def get_next_game():
-    session = Session(profile_name="Default")
+    session = Session(aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"), aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"))
     s3 = session.resource("s3")
     bckt = s3.Bucket("lol-stats-games-queue")
     games = [game for game in bckt.objects.all()]
@@ -53,7 +53,7 @@ def get_next_game():
     return game
 
 def get_player_or_game():
-    session = Session(profile_name="Default")
+    session = Session(aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"), aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"))
     s3 = session.resource("s3")
     player_queue = s3.Bucket("lol-stats-players-queue")
     players = sum(1 for _ in player_queue.objects.all())
@@ -76,7 +76,7 @@ def explore_player(accountId, begin=0):
     response = s.get(f'{API_URL}/lol/match/v4/matchlists/by-account/{accountId}?queue=420&beginTime={begin_time}&beginIndex={begin}', headers=HEADERS)
     if response.status_code == 200:
         body = response.json()
-        session = Session(profile_name="Default")
+        session = Session(aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"), aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"))
         s3 = session.resource("s3")
         for match in body['matches']:
             pass
@@ -128,7 +128,7 @@ def explore_game(gameId):
             cursor.close()
             for player in body['participantIdentities']:
                 player = player['player']
-                session = Session(profile_name="Default")
+                session = Session(aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"), aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"))
                 s3 = session.resource("s3")
                 s3.Bucket('lol-stats-players-queue').put_object(Key=player['accountId'], Body='')
         elif response.status_code == 429:
